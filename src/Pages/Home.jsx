e7199c362281a5871f1011330e810ac1e3a2413b
Larryasure <olascolanre@gmail.com>
2026-01-24 20:42:13 +0100

    New one again

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      Home
-
-    </div>
-  )
+    <>
+      <div className=" min-h-screen px-8 py-16  max-w-7xl mx-auto ">
+        <h1 className="tex-4xl font-bold mb-4 ">Welcome to my Blog</h1>
+        <p className="text-gray-600 max-w-xl ">
+          Articles, tutorials, and ideas on tech, lifestyle, and more.
+        </p>
+      </div>
+    </>
+  );
 }

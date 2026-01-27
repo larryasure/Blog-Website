dfeb73413250f2aa74a47f1142a87ddb2f34b96e
Larryasure <olascolanre@gmail.com>
2026-01-27 11:01:09 +0100

    new one

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import Rootlayout from "./Roots/Rootlayout";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Error404 from "./Pages/Error404";
-import Blogs from "./Pages/Blogs";
-import BlogPost from "./Pages/BlogPost";
-import Contact from "./Pages/Contact";
-import Category from "./Pages/Category";
-import CategoryPosts from "./Pages/CategoryPosts";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Rootlayout,
-
-    children: [
-      { index: true, Component: Home },
-      { path: "about", Component: About },
-      { path: "blogs", Component: Blogs },
-      { path: "blogs/:slug", Component: BlogPost },
-      { path: "contact", Component: Contact },
-      { path: "category", Component: Category },
-      { path: "category/:slug", Component: CategoryPosts },
-      { path: "*", Component: Error404 },
-    ],
-  },
-]);
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
-);
+)
